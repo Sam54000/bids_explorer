df4d2421f9dcce7f6e0b73da40502d0c20805e04
Samuel Louviot <sam.louviot@gmail.com>
2025-02-07 13:59:49 -0500

    improve tests

--- src/bids_explorer/elements/electrodes.py
+++ src/bids_explorer/elements/electrodes.py
@@ -10,7 +10,27 @@ from bids_explorer.utils.parsing import parse_bids_filename
 
 @dataclass
 class Electrodes:
-    def from_file(self, electrode_file: Path, channels_file: Path):
+    """Class to handle the electrodes data and metadata.
+
+    This is to facilitate plotting electrode data such as impedances,
+    positions, or quality across a cohort of subjects.
+    """
+
+    def from_file(
+        self, electrode_file: Path, channels_file: Path
+    ) -> "Electrodes":
+        """Instantiate an Electrodes object from an 2 csv file.
+
+        The first file contains the electrode data and the second file
+        contains the channels data according to the BIDS specifications.
+
+        Args:
+            electrode_file (Path): Path to the electrode csv file.
+            channels_file (Path): Path to the channels csv file.
+
+        Returns:
+            Electrodes: An Electrodes object.
+        """
         electrode_data = pd.read_csv(electrode_file, sep="\t")
         channels_data = pd.read_csvs(channels_file, sep="\t")
         entities = parse_bids_filename(electrode_file)
@@ -29,7 +49,18 @@ class Electrodes:
 
         return self
 
-    def from_dataframe(self, dataframe=pd.DataFrame):
+    def from_dataframe(self, dataframe: pd.DataFrame) -> "Electrodes":
+        """Instantiate an Electrodes object from a pandas DataFrame.
+
+        Usually used when the data is already taken care from another process.
+
+        Args:
+            dataframe (pd.DataFrame): A pandas DataFrame containing the
+            electrodes data.
+
+        Returns:
+            Electrodes: An Electrodes object.
+        """
         self.data = dataframe
         self.spaces = dataframe["space"].unique()
         self.subjects = dataframe["subject"].unique()
@@ -44,4 +75,9 @@ class Electrodes:
 
 
 class ElectrodesCollection:
-    electrodes: list[Electrodes]
+    """Class to handle a collection of Electrodes objects.
+
+    This is to facilitate the processing of multiple electrodes data.
+    """
+
+    pass
